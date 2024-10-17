9a73f401e77a590687b27b2dca334b21ba5db434
devpablocristo <devpablocristo@gmail.com>
2024-10-17 11:19:18 -0300

    cosas

--- middleware/gin/validate-credentials.go
+++ middleware/gin/validate-credentials.go
@@ -16,17 +16,17 @@ const (
 // ValidateCredentials middleware para validar el payload del login
 func ValidateCredentials() gin.HandlerFunc {
 	return func(ctx *gin.Context) {
-		var loginRequest sdktypes.LoginCredentials
+		var creds sdktypes.LoginCredentials
 
 		// Manejo del binding y retorno de error en caso de fallo
-		if err := ctx.ShouldBindJSON(&loginRequest); err != nil {
+		if err := ctx.ShouldBindJSON(&creds); err != nil {
 			ctx.JSON(http.StatusBadRequest, gin.H{"error": errMissingCredentials})
 			ctx.Abort()
 			return
 		}
 
 		// Guardar los datos validados en el contexto para el siguiente handler
-		ctx.Set("loginRequest", loginRequest)
+		ctx.Set("Credentials:", creds)
 		ctx.Next()
 	}
 }
